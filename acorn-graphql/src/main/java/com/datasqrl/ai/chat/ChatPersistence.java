82a3a1b5a58bacb53055e0f722c33e7b18064bbe
Matthias Broecheler <mbroecheler@users.noreply.github.com>
2024-11-24 20:37:32 -0800

    Fixed refactoring, adding back other models. Not yet tested but compiles

--- acorn-graphql/src/main/java/com/datasqrl/ai/chat/ChatPersistence.java
+++ acorn-graphql/src/main/java/com/datasqrl/ai/chat/ChatPersistence.java
@@ -8,6 +8,21 @@ import lombok.NonNull;
 
 public interface ChatPersistence {
 
+  ChatPersistence NONE = new ChatPersistence() {
+
+    @Override
+    public CompletableFuture<String> saveChatMessage(@NonNull Object message,
+        @NonNull Context context) {
+      return CompletableFuture.completedFuture("disabled");
+    }
+
+    @Override
+    public <ChatMessage> List<ChatMessage> getChatMessages(@NonNull Context context, int limit,
+        @NonNull Class<ChatMessage> clazz) throws IOException {
+      return List.of();
+    }
+  };
+
   public CompletableFuture<String> saveChatMessage(@NonNull Object message, @NonNull Context context);
 
   public <ChatMessage> List<ChatMessage> getChatMessages(
